6502dd67ee6526e0b3470f878e20b62993926627
tuananhcdv2 <139940037+tuananhcdv2@users.noreply.github.com>
2024-12-03 15:49:27 +0700

    Add files via upload

---
+++ daoxmr2.cmd
@@ -0,0 +1,2 @@
+xmrig.exe -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u XMR:46WDpkRdcqVLtcpXuPeeFiiTgvKPh7XQx7kc1R7KuHC3hYwPWUcuehpEhnc5vji2zF9PFN2MPZfw35XSwFm1xaQuV3Vdtqu -p tuananh --worker worker2
+pause
