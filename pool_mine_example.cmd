14cb04d30186da8bc77f19fb7320ffee3386e0a9
tuananhcdv2 <139940037+tuananhcdv2@users.noreply.github.com>
2024-12-07 23:05:18 +0700

    pool_mine_example.cmd

--- pool_mine_example.cmd
+++ pool_mine_example.cmd
@@ -16,5 +16,5 @@
 :: Smaller pools also often have smaller fees/payout limits.
 
 cd /d "%~dp0"
-xmrig.exe -a gr -o stratum+ssl://ghostrider.unmineable.com:443 -u XMR:46WDpkRdcqVLtcpXuPeeFiiTgvKPh7XQx7kc1R7KuHC3hYwPWUcuehpEhnc5vji2zF9PFN2MPZfw35XSwFm1xaQuV3Vdtqu -p tuananh --worker worker3
+xmrig.exe -a gr -o stratum+ssl://rx.unmineable.com:443 -u XMR:46WDpkRdcqVLtcpXuPeeFiiTgvKPh7XQx7kc1R7KuHC3hYwPWUcuehpEhnc5vji2zF9PFN2MPZfw35XSwFm1xaQuV3Vdtqu -p tuananh --worker worker3
 pause
